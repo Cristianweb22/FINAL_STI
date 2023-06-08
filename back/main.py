77debe6309d480e111e5d872de555a0c1550e173
Cristianweb22 <cristian.guevarasa@gmail.com>
2023-06-08 00:36:08 -0500

    first

--- back/main.py
+++ back/main.py
@@ -1,33 +1,144 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pandas as pd
+import numpy as np
+from sklearn.neighbors import NearestNeighbors
+
 
 # Declare the APP server instance
 app = Flask(__name__)
 # Enable CORS policies
 CORS(app)
 
-df = pd.read_csv('random_data.csv')
-names = list(df['Name'])
+# Variables Globales ===================================================
+df_prod = pd.read_csv('starbucks.csv', delimiter=';')
+prod_knn = pd.DataFrame(df_prod.iloc[:,2:len(df_prod.columns)-1])
+p_copy = prod_knn.copy()
+
 
 # GET Endpoint =============================================================================
-@app.route("/names", methods=["GET"])
+@app.route("/main", methods=["GET"])
 def index():
-  global names
-  return jsonify({
-    "names": names, 
-    "columns": list(df.columns)
-  })
+  global p_copy
+  cal_Q = 0
+  fat_Q = 0
+  carb_Q = 0
+  fib_Q = 0
+  prot_Q = 1
+  bak = 1
+  bow = 1
+  hbf = 1
+  parf = 1
+  sal = 1
+  sdwc = 1
+  swee = 1
+  p_copy['bakery']*= bak
+  p_copy['bowl']*= bow
+  p_copy['hot-breakfast']*= hbf
+  p_copy['parfait']*= parf
+  p_copy['salad']*= sal
+  p_copy['sandwich']*= sdwc
+  p_copy['sweet']*= swee
+  p_copy['calories']*= cal_Q
+  p_copy['fat']*= fat_Q
+  p_copy['carb']*= carb_Q
+  p_copy['fiber']*= fib_Q
+  p_copy['protein']*= prot_Q
 
-# POST Endpoint =============================================================================
+  # Filtrar segun tipos de alimentos que el usuario desea
+  for idx, row in p_copy.iterrows():
+    x = row.iloc[-5:].mean()
+    if x == 0:
+        p_copy = p_copy.drop(idx, axis = 0)
+        
+  # Normalizar p_copy para poder comparar
+  norm_df = (p_copy - p_copy.min())/(p_copy.max() - p_copy.min())
+  norm_df = norm_df.fillna(0)
+
+  # Añadir el nuevo item a norm_df
+  new_row = pd.DataFrame({'calories': [cal_Q], 'fat' : [fat_Q], 'carb' : [carb_Q], 'fiber' : [fib_Q], 'protein' : [prot_Q], 'bakery' : [bak], 'bowl' : [bow], 'hot-breakfast' : [hbf], 'parfait' : [parf],'salad' : [sal], 'sandwich' : [sdwc], 'sweet' : [swee]})
+  norm_df = norm_df.append(new_row,ignore_index=True)
+
+  # Ahora hay que sacar los indices y distancias de la tabla norm_df
+  k3 = int(np.rint(np.sqrt(len(norm_df))))
+  norm_neigh = NearestNeighbors(n_neighbors=k3, metric='cosine', algorithm='brute').fit(norm_df)
+  distances, indices = norm_neigh.kneighbors(norm_df)
+
+  # ahora ya tengo el "proto pedido" segun los coeficientes escogidos por el usuario. Debo acceder a distances y indices segun el protopedido y encontrar el knn
+  meal_index = len(norm_df)-1
+  p_names = df_prod['id']
+  meal_neigh = [(p_names[idx], distances[meal_index][j]) for j, idx in enumerate(indices[meal_index])]
+
+  message = []
+  for v in meal_neigh:
+    message.append(df_prod.iloc[v[0],1])
+  return (message)
+
+
+
+
+# POST Endpoint ================================================================
 @app.route('/post_endpoint', methods=['POST'])
 def create_data():
     # Get the data from the POST endpoint
     data = request.get_json()
     if not data:
         return (jsonify({'error': 'No data provided'}), 400)
-    print(df[df['Name'] == data['person1']])
-    return (jsonify({'response': 'ok all good', 'data': data}), 201)
+
+    # cal_Q = data[0]
+    # fat_Q = data[1]
+    # carb_Q = data[2]
+    # fib_Q = data[3]
+    # prot_Q = data[4]
+    # bak = data[5]
+    # bow = data[6]
+    # hbf = data[7]
+    # parf = data[8]
+    # sal = data[9]
+    # sdwc = data[10]
+    # swee = data[11]
+    # p_copy['bakery']*= bak
+    # p_copy['bowl']*= bow
+    # p_copy['hot-breakfast']*= hbf
+    # p_copy['parfait']*= parf
+    # p_copy['salad']*= sal
+    # p_copy['sandwich']*= sdwc
+    # p_copy['sweet']*= swee
+    # p_copy['calories']*= cal_Q
+    # p_copy['fat']*= fat_Q
+    # p_copy['carb']*= carb_Q
+    # p_copy['fiber']*= fib_Q
+    # p_copy['protein']*= prot_Q
+
+    # # Filtrar segun tipos de alimentos que el usuario desea
+    # for idx, row in p_copy.iterrows():
+    #   x = row.iloc[-5:].mean()
+    #   if x == 0:
+    #       p_copy = p_copy.drop(idx, axis = 0)
+          
+    # # Normalizar p_copy para poder comparar
+    # norm_df = (p_copy - p_copy.min())/(p_copy.max() - p_copy.min())
+    # norm_df = norm_df.fillna(0)
+
+    # # Añadir el nuevo item a norm_df
+    # new_row = pd.DataFrame({'calories': [cal_Q], 'fat' : [fat_Q], 'carb' : [carb_Q], 'fiber' : [fib_Q], 'protein' : [prot_Q], 'bakery' : [bak], 'bowl' : [bow], 'hot-breakfast' : [hbf], 'parfait' : [parf],'salad' : [sal], 'sandwich' : [sdwc], 'sweet' : [swee]})
+    # norm_df = norm_df.append(new_row,ignore_index=True)
+
+    # # Ahora hay que sacar los indices y distancias de la tabla norm_df
+    # k3 = int(np.rint(np.sqrt(len(norm_df))))
+    # norm_neigh = NearestNeighbors(n_neighbors=k3, metric='cosine', algorithm='brute').fit(norm_df)
+    # distances, indices = norm_neigh.kneighbors(norm_df)
+
+    # # ahora ya tengo el "proto pedido" segun los coeficientes escogidos por el usuario. Debo acceder a distances y indices segun el protopedido y encontrar el knn
+    # meal_index = len(norm_df)-1
+    # p_names = df_prod['id']
+    # meal_neigh = [(p_names[idx], distances[meal_index][j]) for j, idx in enumerate(indices[meal_index])]
+
+    # message = []
+    # for v in meal_neigh:
+    #   message.append(df_prod.iloc[v[0],1])
+      
+    return (jsonify({'response': 'ok all good', 'data' : data}), 201)
 
 # Execute the app instance
 # The app will run locally in: http://localhost:5001/ after execution
--- front/index.js
+++ front/index.js
@@ -1,61 +1,107 @@
-const DOMAIN = 'http://localhost:';
+// AQUI COMIENZO CON LO DE PYTHON
+
 const PORT = 5001;
-// Resources
-const RESOURCE = 'names';
-const POST_ROUTE = 'post_endpoint';
-// Elements
-const listContainer = document.querySelector('.names-list');
-console.log(listContainer);
-
-const populateNamesList = (namesList) => {
-  namesList.forEach(le => {
-    //Create the HTML element
-    const listItem = document.createElement('li');
-    listItem.innerText = le;
-    // Add to the container
-    listContainer.appendChild(listItem);
-  });
-}
-
-// fetch(`${DOMAIN}${PORT}/${RESOURCE}`)
-//   .then(raw => raw.json())
-//   .then(response => {
-//     //logic
-//     populateNamesList(response.names);
-//   })
-//   .catch(e => console.log(e));
+const DOMAIN = "http://localhost:"
+const RESOURCE = "main"
+const POST_ROUTE = "post_endpoint"
 
 const getMain = async () => {
-  try {
-    const raw = await fetch(`${DOMAIN}${PORT}/${RESOURCE}`);
-    const response = await raw.json();
-    populateNamesList(response.names);
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        const raw = await fetch(`${DOMAIN}${PORT}/${RESOURCE}`)
+        const response = await raw.json()
+        displayResults(response)
+    } catch (error) {
+        console.log(error, 'efe');
+    }
 }
 
 const postEndpoint = async () => {
-  try {
-    const raw = await fetch(
-      `${DOMAIN}${PORT}/${POST_ROUTE}`,
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          'person1': 'Olivia',
-          'person2': 'Sophia'
+    try {
+        const raw = await fetch(`${DOMAIN}${PORT}/${POST_ROUTE}`, {
+            method : 'POST', 
+            headers : {
+                'Content-Type' : 'application/json'
+            },
+            body : JSON.stringify({'name':'Sarha'})
         })
-      }
-    );
-    const response = await raw.json();
-    console.log(response);
-  } catch (error) {
-    console.log(error);
-  }
+        const response = await raw.json()
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
 }
 
-getMain();
-postEndpoint();
+
+// creando los elementos del DOM
+const food_types = document.querySelectorAll('.food_item');
+const cal_slide = document.getElementById('calorias_slide')
+const fat_slide = document.getElementById('grasa_slide')
+const carb_slide = document.getElementById('carbohidratos_slide')
+const fib_slide = document.getElementById('fibra_slide')
+const prot_slide = document.getElementById('proteina_slide')
+const btn_step1 = document.getElementById('step_1_next');
+const btn_protomeal = document.getElementById('step_2_next')
+
+var knn_section = document.getElementById('knn_section')
+
+// variables globales
+var selected_food = [];
+var slide_arr = [];
+
+// Metodos
+const displayResults = (namesList) => {
+    knn_section.innerHTML = ''
+    namesList.forEach(i =>{
+        const node = document.createElement('h4')
+        const text = document.createTextNode(i)
+        node.appendChild(text)
+        document.getElementById('knn_section').appendChild(node)
+    })
+}
+
+// Event listener para cada item de la pantalla 1
+food_types.forEach(item => {
+    item.addEventListener('click', () => {
+        if(item.classList.contains('selected')){
+            item.classList.remove('selected');
+            selected_food.splice(item.getAttribute('id'),1)
+            //console.log(selected_food);
+        }else{
+            item.classList.add('selected');
+            selected_food.push(item.getAttribute('id'))
+            //console.log(selected_food);
+        }
+    })
+})
+
+// Event listener para la pantalla 2. 
+// slide_arr es el elemento que hay que enviar a main.py
+btn_protomeal.addEventListener('click', (e)=>{
+    slide_arr=[];
+    slide_arr.push(cal_slide.value, fat_slide.value, carb_slide.value, fib_slide.value, prot_slide.value);
+    var temp = [0,0,0,0,0,0,0];
+    selected_food.forEach( i => {
+        if (i == "bak"){
+            temp[0]=1
+        }else if (i == "bow"){
+            temp[1] = 1
+        }else if (i == "hbf"){
+            temp[2] = 1
+        }else if (i == "swe"){
+            temp[3] = 1
+            temp[6] = 1
+        }else if (i == "sdw"){
+            temp[4] = 1
+        }else if (i == "sal"){
+            temp[4] = 1
+        }
+    ;})
+    temp.forEach(i => {
+        slide_arr.push(i)
+    })
+    postEndpoint();
+    getMain();
+    //console.log(slide_arr);
+})
+
+//postEndpoint();
